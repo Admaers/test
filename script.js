5d905ff3cbe35c77060683f38a6a815be7919c2a
Ivan <ctb-3@ya.ru>
2020-01-06 15:03:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use script';
+
+var number = 5;
+var obj = {};
+let persone = {
+  name: "Tom",
+  age: "20",
+  isMarried: false
+};
+var arr = ["orange.jpg","apple.inc"];
+
+let incr = 10,
+    decr = 10;
